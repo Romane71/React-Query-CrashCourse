86a57d25bb68efa4c7a3cee1106e3590e91404a2
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-10 17:20:10 +0000

    final

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,39 +1,53 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { getPost } from "./api/posts";
+import { CreatePost } from "./CreatePost";
+import Post from "./Post";
+import { PostListInfinite } from "./PostListInfinite";
+import { PostListPaginated } from "./PostListPaginated";
+import PostsList1 from "./PostsList1";
+import PostsList2 from "./PostsList2";
 
-const POSTS = [
-  { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
-];
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(<PostsList1 />);
+  const queryClient = useQueryClient();
 
-// /posts-> ["posts"]
-// /posts1 -> ["posts", post.id]
-// /posts?authorId=1 -> ["posts", { author.Id}]
-// /posts/2/comments -> ["posts", post.id, "comments"]
-
-function App() {
-  const postsQuery = useQuery({
-    queryKey: ["posts"],
-    queryFn: (obj) =>
-      wait(1000).then(() => {
-        console.log(obj);
-        return [...POSTS];
-      }),
-  });
-
-  if (postsQuery.isLoading) return <h1>Loading...</h1>;
-  if (postsQuery.isError) {
-    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  function onHoverPostOneLink() {
+    queryClient.prefetchQuery({
+      queryKey: ["posts", 1],
+      queryFn: () => getPost(1),
+    });
   }
+
   return (
     <div>
-      {postsQuery.data.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
+      <button onClick={() => setCurrentPage(<PostsList1 />)}>
+        Posts List 1
+      </button>
+      <button onClick={() => setCurrentPage(<PostsList2 />)}>
+        Posts List 2
+      </button>
+      <button
+        onMouseEnter={onHoverPostOneLink}
+        onClick={() => setCurrentPage(<Post id={1} />)}
+      >
+        First Post
+      </button>
+      <button
+        onClick={() =>
+          setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)
+        }
+      >
+        New Post
+      </button>
+      <button onClick={() => setCurrentPage(<PostListPaginated />)}>
+        Post List Paginated
+      </button>
+      <button onClick={() => setCurrentPage(<PostListInfinite />)}>
+        Post List Infinite
+      </button>
+      <br />
+      {currentPage}
     </div>
   );
 }
-
-function wait(duration) {
-  return new Promise((resolve) => setTimeout(resolve, duration));
-}
-export default App;
