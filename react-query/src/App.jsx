c759b40e10dcc239d86124872006aa053fb719a5
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-10 16:04:03 +0000

    first query

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -2,7 +2,7 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 
 const POSTS = [
   { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
+  { id: 2, title: "post 2" },
 ];
 
 function App() {
@@ -10,14 +10,19 @@ function App() {
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
   });
-
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
-  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
-  return <h1>TanStack Query</h1>;
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
+  return (
+    <div>
+      {postsQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 }
-
 function wait(duration) {
   return new Promise((resolve) => setTimeout(resolve, duration));
 }
-
 export default App;
